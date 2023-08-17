436a38237f7d4e28cd1a00b1975fa19bff8ec710
gabrielestefono <gabrielestefono@hotmail.com>
2023-08-16 22:39:42 -0300

    Criando rotas

--- Aula01/app.py
+++ Aula01/app.py
@@ -1,15 +1,21 @@
+# Fazendo importações
+
 from flask import Flask
 from flask_restful import Resource, Api
+
+# Instanciando o Flask
 
 app = Flask(__name__)
 api = Api(app)
 
 
+# Criando a classe Hoteis
 class Hoteis(Resource):
     def get(self):
         return {'hoteis': 'Meus Hoteis'}
 
 
+# Adicionando a rota
 api.add_resource(Hoteis, "/hoteis")
 if __name__ == "__main__":
     app.run(debug=True)
